7f930c76d43baf5e7d7b03a960a4b409387533d4
Jean Carlos Coelho <jeancoelho@live.com>
2025-04-13 16:13:59 -0400

    feat: adiciona cache com Redis e configura rede host para Docker

--- translate_api.py
+++ translate_api.py
@@ -1,16 +1,28 @@
-
-from fastapi import FastAPI, Request, HTTPException, Header
+from fastapi import FastAPI, HTTPException, Header
 from pydantic import BaseModel
 import argostranslate.package
 import argostranslate.translate
 import os
+import redis
+import hashlib
 from dotenv import load_dotenv
 
 load_dotenv()
-
 app = FastAPI()
 
-API_KEY = os.getenv("ARGOS_API_KEY", "default-insecure-key")
+API_KEY = os.getenv("ARGOS_API_KEY", "bb52f440-f0e5-4a48-aecb-c8a1d7d54bb9")
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
+CACHE_TTL_SECONDS = 60 * 60 * 24 * 30  # 30 dias
+
+r = redis.Redis(
+    host=REDIS_HOST,
+    port=REDIS_PORT,
+    password=REDIS_PASSWORD,
+    decode_responses=True
+)
+
 
 class TranslationRequest(BaseModel):
     text: str
@@ -30,6 +42,14 @@ def translate_text(payload: TranslationRequest, x_api_key: str = Header(None)):
     if x_api_key != API_KEY:
         raise HTTPException(status_code=401, detail="Unauthorized")
 
+    key_hash = hashlib.sha256(f"{payload.from_lang}:{payload.to_lang}:{payload.text}".encode()).hexdigest()
+    cached = r.get(key_hash)
+
+    if cached:
+        return {"translated": cached}
+
     ensure_model(payload.from_lang, payload.to_lang)
     translated = argostranslate.translate.translate(payload.text, payload.from_lang, payload.to_lang)
+
+    r.setex(key_hash, CACHE_TTL_SECONDS, translated)
     return {"translated": translated}
